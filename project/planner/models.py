0f6d6f65f8ff957dfa87a89607b05f00b215d9d3
Paweł Staszczuk <41273017+staszczuk@users.noreply.github.com>
2022-12-29 00:23:15 +0100

    create event model

--- project/planner/models.py
+++ project/planner/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Event(models.Model):
+    date = models.DateField()
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
