799b362fe8adbf137d1429d8cdef5b4ce5b66716
Paweł Staszczuk <41273017+staszczuk@users.noreply.github.com>
2022-12-29 00:36:58 +0100

    update month view and add css

--- project/planner/views.py
+++ project/planner/views.py
@@ -1,10 +1,25 @@
+from calendar import Calendar
+
 from django.shortcuts import render
+
+from .models import Event
 
 # Create your views here.
 
 
 def month(request, year, month):
+    events = Event.objects.filter(date__year=year, date__month=month)
+    calendar = Calendar()
+    days = {}
+    for date in calendar.itermonthdates(year, month):
+        days.update({f'{date}': {
+            'date': date,
+            'events': [],
+        }})
+    for event in events:
+        days.get(f'{event.date}').get('events').append(event)
     return render(request, 'planner/month.html', {
         'year': year,
         'month': month,
+        'days': days,
     })
