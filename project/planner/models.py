46ede757808997ba41414e680e1d96084c285b23
Paweł Staszczuk <41273017+staszczuk@users.noreply.github.com>
2023-01-14 02:12:12 +0100

    add event time

--- project/planner/models.py
+++ project/planner/models.py
@@ -1,3 +1,5 @@
+from datetime import time
+
 from django.db import models
 
 # Create your models here.
@@ -5,6 +7,7 @@ from django.db import models
 
 class Event(models.Model):
     date = models.DateField()
+    time = models.TimeField(default=time(0, 0))
     name = models.CharField(max_length=128)
 
     def __str__(self):
--- project/planner/views.py
+++ project/planner/views.py
@@ -17,41 +17,54 @@ def month(request, year, month):
     for index, week in enumerate(cal.monthdatescalendar(year, month)):
         weeks.append([])
         for day in week:
-            weeks[index].append({
-                'date': day,
-                'events': Event.objects.filter(date=day),
-            })
+            weeks[index].append(
+                {
+                    "date": day,
+                    "events": Event.objects.filter(date=day),
+                }
+            )
     prev = utils.prev_month(year, month)
     next = utils.next_month(year, month)
-    return render(request, 'planner/month.html', {
-        'year': year,
-        'month': month,
-        'weeks': weeks,
-        'prev': prev,
-        'next': next,
-    })
+    return render(
+        request,
+        "planner/month.html",
+        {
+            "year": year,
+            "month": month,
+            "weeks": weeks,
+            "prev": prev,
+            "next": next,
+        },
+    )
 
 
 def day(request, year, month, day):
-    events = Event.objects.filter(
-        date__year=year, date__month=month, date__day=day)
-    return render(request, 'planner/day.html', {
-        'year': year,
-        'month': month,
-        'day': day,
-        'events': events,
-    })
+    events = Event.objects.filter(date__year=year, date__month=month, date__day=day)
+    return render(
+        request,
+        "planner/day.html",
+        {
+            "year": year,
+            "month": month,
+            "day": day,
+            "events": events,
+        },
+    )
 
 
 def add_event(request, year, month, day):
-    event = Event(date=date(year, month, day),
-                  name=request.POST['event--name'])
+    event = Event(
+        date=date(year, month, day),
+        time=request.POST["event--time"],
+        name=request.POST["event--name"],
+    )
     event.save()
-    return HttpResponseRedirect(reverse('planner:day', args=(year, month, day)))
+    return HttpResponseRedirect(reverse("planner:day", args=(year, month, day)))
 
 
 def delete_event(request, year, month, day):
-    event = get_object_or_404(Event, date=date(
-        year, month, day), name=request.POST['event--name'])
+    event = get_object_or_404(
+        Event, date=date(year, month, day), name=request.POST["event--name"]
+    )
     event.delete()
-    return HttpResponseRedirect(reverse('planner:day', args=(year, month, day)))
+    return HttpResponseRedirect(reverse("planner:day", args=(year, month, day)))
